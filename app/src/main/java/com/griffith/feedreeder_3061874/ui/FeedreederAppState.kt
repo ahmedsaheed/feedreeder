52b0dc50df5b4228a91b840a7a3681ec39edde7e
ahmed <ahmedsaheed2@outlook.com>
2023-11-09 02:09:52 +0000

    core(nav): Initial setup app of navigation

---
+++ app/src/main/java/com/griffith/feedreeder_3061874/ui/FeedreederAppState.kt
@@ -0,0 +1,72 @@
+package com.griffith.feedreeder_3061874.ui
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.Uri
+import android.os.Build
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Reader : Screen("reader/{episodeUri}") {
+        fun createRoute(episodeUri: String) = "reader/$episodeUri"
+    }
+}
+
+@Composable
+
+fun rememberFeedReederAppState(
+    navController: NavHostController = rememberNavController(),
+    context: Context = LocalContext.current
+) = remember(navController, context) {
+    FeedReederAppState(navController, context)
+}
+class FeedReederAppState(
+    val navController: NavHostController,
+    private val context: Context
+){
+    var isOnline by mutableStateOf(checkIfOnline())
+        private set
+
+    fun refreshOnline() {
+        isOnline = checkIfOnline()
+    }
+
+    fun navigateBack() { navController.popBackStack() }
+
+
+        private fun checkIfOnline(): Boolean {
+        val cm = getSystemService(context, ConnectivityManager::class.java)
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
+            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                    capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        } else {
+            cm?.activeNetworkInfo?.isConnectedOrConnecting == true
+
+        }
+    }
+
+    fun navigateToReader(episodeUri: String, from: NavBackStackEntry) {
+        if (from.lifecycleIsResumed()) {
+            val encodedUri = Uri.encode(episodeUri)
+            navController.navigate(Screen.Reader.createRoute(encodedUri))
+        }
+    }
+}
+
+private fun NavBackStackEntry.lifecycleIsResumed() =
+    this.lifecycle.currentState == Lifecycle.State.RESUMED
