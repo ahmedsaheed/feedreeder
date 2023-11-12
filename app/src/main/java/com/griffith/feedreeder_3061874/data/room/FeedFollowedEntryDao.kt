c5e8e70b8bf9e4eb309e2cdd29af3d8ccf87805b
ahmed <ahmedsaheed2@outlook.com>
2023-11-12 14:28:01 +0000

    core(`DAO`): setup feedreeder database

---
+++ app/src/main/java/com/griffith/feedreeder_3061874/data/room/FeedFollowedEntryDao.kt
@@ -0,0 +1,16 @@
+package com.griffith.feedreeder_3061874.data.room
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.griffith.feedreeder_3061874.data.FeedFollowedEntry
+
+@Dao
+abstract class FeedFollowedEntryDao: BaseDao<FeedFollowedEntry> {
+   @Query("DELETE FROM feed_followed_entries WHERE feed_uri = :feedUri")
+   abstract suspend fun deleteWithFeedUri(feedUri : String)
+
+   @Query("SELECT COUNT(*) FROM feed_followed_entries WHERE feed_uri = :feedUri")
+   protected abstract suspend fun feedFollowRowCount(feedUri: String) : Int
+
+   suspend fun isFeedFollowed(feedUri: String) : Boolean  = feedFollowRowCount(feedUri) > 0
+}
---
+++ app/src/main/java/com/griffith/feedreeder_3061874/data/room/FeedreederDatabase.kt
@@ -0,0 +1,32 @@
+package com.griffith.feedreeder_3061874.data.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.griffith.feedreeder_3061874.data.Category
+import com.griffith.feedreeder_3061874.data.Episode
+import com.griffith.feedreeder_3061874.data.FeedCategoryEntry
+import com.griffith.feedreeder_3061874.data.FeedCollection
+import com.griffith.feedreeder_3061874.data.FeedFollowedEntry
+
+@Database(
+    entities = [
+        FeedCollection::class,
+        Episode::class,
+        FeedCategoryEntry::class,
+        Category::class,
+        FeedFollowedEntry::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DateTimeTypeConverters::class)
+abstract class FeedreederDatabase : RoomDatabase() {
+    abstract fun feedsDao() : FeedsDao
+    abstract fun episodesDao(): EpisodesDoa
+    abstract fun categoriesDao(): CategoriesDao
+    abstract fun feedCategoryEntryDao() : FeedCategoryEntryDao
+    abstract fun transactionRunnerDao(): TransactionRunnerDao
+    abstract fun feedFollowedEntryDao(): FeedFollowedEntryDao
+}
