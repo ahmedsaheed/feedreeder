937d9ca3621688db4e769fa5f3ae675f0cd73a4c
ahmed <ahmedsaheed2@outlook.com>
2023-11-25 21:30:52 +0000

    chore(build): fix buildscripts

--- buildscripts/init.gradle.kts
+++ buildscripts/init.gradle.kts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
 val ktlintVersion = "0.46.1"
 
 initscript {
@@ -42,9 +26,6 @@ allprojects {
                             mapOf(
                                 "ktlint_code_style" to "android",
                                 "ij_kotlin_allow_trailing_comma" to true,
-                                // These rules were introduced in ktlint 0.46.0 and should not be
-                                // enabled without further discussion. They are disabled for now.
-                                // See: https://github.com/pinterest/ktlint/releases/tag/0.46.0
                                 "disabled_rules" to
                                     "filename," +
                                     "annotation,annotation-spacing," +
